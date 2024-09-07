b012c997446517b7e8249a7c33c52066194e8440
Redplcs <51379496+Redplcs@users.noreply.github.com>
2024-09-07 20:10:58 +0500

    Add avalonia ui project

---
+++ ui/HoleVpn.Client.UI.Main/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HoleVpn.Client.UI.Main;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
