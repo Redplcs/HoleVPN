65628def05eba0c2b69f5ba51d890a7282af35d6
Redplcs <51379496+Redplcs@users.noreply.github.com>
2024-09-08 17:04:52 +0300

    Add dependency injection and contain app and window into them

--- ui/HoleVpn.Client.UI.Main/App.axaml.cs
+++ ui/HoleVpn.Client.UI.Main/App.axaml.cs
@@ -1,14 +1,30 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace HoleVpn.Client.UI.Main;
 
 public partial class App : Application
 {
+    private readonly IServiceProvider _services;
+
     public App()
     {
         Name = "HoleVPN";
+
+        _services = CreateServiceProvider();
+    }
+
+    private ServiceProvider CreateServiceProvider()
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddSingleton(this);
+        collection.AddSingleton<MainWindow>();
+
+        return collection.BuildServiceProvider();
     }
 
     public override void Initialize()
@@ -20,10 +36,8 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow()
-            {
-                Title = Name,
-            };
+            desktop.MainWindow = _services.GetRequiredService<MainWindow>();
+            desktop.MainWindow.Title = Name;
         }
 
         base.OnFrameworkInitializationCompleted();
