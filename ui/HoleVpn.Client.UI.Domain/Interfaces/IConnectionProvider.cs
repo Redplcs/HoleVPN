2f05ba46200fe41362e6e072a55a43bc73cf3e9c
Redplcs <51379496+Redplcs@users.noreply.github.com>
2024-09-08 15:00:09 +0300

    Add connection state enum and provider to connect and state getter

---
+++ ui/HoleVpn.Client.UI.Domain/Interfaces/IConnectionProvider.cs
@@ -0,0 +1,11 @@
+﻿using HoleVpn.Client.UI.Domain.ValueObjects;
+using System.Net;
+
+namespace HoleVpn.Client.UI.Domain.Interfaces;
+
+public interface IConnectionProvider
+{
+    ValueTask<bool> ConnectAsync(EndPoint endpoint, CancellationToken cancellationToken);
+
+    ConnectionState GetConnectionState();
+}
---
+++ ui/HoleVpn.Client.UI.Domain/ValueObjects/ConnectionState.cs
@@ -0,0 +1,10 @@
+﻿namespace HoleVpn.Client.UI.Domain.ValueObjects;
+
+public enum ConnectionState
+{
+    None,
+    Connecting,
+    Connected,
+    Disconnecting,
+    Disconnected,
+}
